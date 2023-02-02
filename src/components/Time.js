e6d4bf7c73d8f823e72fd5a23b3cea46b879b653
Fanush <fgabriel.mihailescu@gmail.com>
2023-02-02 23:09:14 +0200

    last one, I promise

--- src/components/Time.js
+++ src/components/Time.js
@@ -8,19 +8,19 @@ export function Time(props){
         <div className="time-container">
             <div className='days'>
                 <p>{days}</p>
-                <span>{days <= 1 ? "Day" : "Days"}</span>
+                <span>{days == 1 ? "Day" : "Days"}</span>
             </div>
             <div className='hours'>
                 <p>{hours}</p>
-                <span>{hours <= 1 ? "Hour" : "Hours"}</span>
+                <span>{hours == 1 ? "Hour" : "Hours"}</span>
             </div>
             <div className='minutes'>
                 <p>{minutes}</p>
-                <span>{minutes <= 1 ? "Minute" : "Minutes"}</span>
+                <span>{minutes == 1 ? "Minute" : "Minutes"}</span>
             </div>
             <div className='seconds'>
                 <p>{seconds}</p>
-                <span>{seconds <= 1 ? "Second" : "Seconds"}</span>
+                <span>{seconds == 1 ? "Second" : "Seconds"}</span>
             </div>
         </div>
     )
