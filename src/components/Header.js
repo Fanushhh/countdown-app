5e20f39164a1ec2566c9b678c3fd18f122c112ab
Fanush <fgabriel.mihailescu@gmail.com>
2023-02-02 22:59:52 +0200

    last commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ export const Header = () => {
         <div className="header">
             <h1>Hello there!</h1>
             <p>It's almost the end of 2022, have you acomplished your goals yet?</p>
-            <p>No? What do you mean no? Get to work you <span ref={ref} className='gif-proc' onClick={showGif} >fool!</span></p>
+            <p>No? What do you mean no? Get to work you <span ref={ref} className='gif-proc' onClick={showGif} >fool! (click here)</span></p>
             <img src="https://media1.giphy.com/media/W3Ch3vjHi5FGefDT0G/giphy.gif?cid=790b7611baaebf16d8a9257435361678780354c5a57a4a6f&rid=giphy.gif&ct=g" className={isClicked ? 'show' : 'hide'} />
         </div>
     )
